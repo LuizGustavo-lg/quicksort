ded1824f6aadde8f1bd9489a4e5273da030d2130
= <=>
2024-09-21 20:00:27 -0300

    geraçao arquivo de dados

--- gerar_arq_dados.py
+++ gerar_arq_dados.py
@@ -1,5 +1,10 @@
 import random
 
+QNT_VAL = 100
+
 with open("dados.txt", "w") as dados:
-    for i in range(1000):
+    for i in range(QNT_VAL-1):
         dados.write(str(random.randint(0, 100)) + '\n')
+    dados.write(str(random.randint(0, 100)))
+
+print("***Arquivo de dados gerado***")
