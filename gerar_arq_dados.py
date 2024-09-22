01b7c2b47f586e56c19277428a88b19efff86099
= <=>
2024-09-22 09:33:05 -0300

    novo metodo de criaçao de dados

--- gerar_arq_dados.py
+++ gerar_arq_dados.py
@@ -1,10 +1,16 @@
 import random
 
-QNT_VAL = 100
+QNT_VAL = 500
+SORT = False
 
 with open("dados.txt", "w") as dados:
-    for i in range(QNT_VAL-1):
-        dados.write(str(random.randint(0, 100)) + '\n')
-    dados.write(str(random.randint(0, 100)))
+    if SORT:
+        for i in range(QNT_VAL-1):
+            dados.write(str(i) + '\n')
+        dados.write(str(700))
+    else:
+        for i in range(QNT_VAL-1):
+            dados.write(str(random.randint(0, 100)) + '\n')
+        dados.write(str(random.randint(0, 100)))
 
 print("***Arquivo de dados gerado***")
